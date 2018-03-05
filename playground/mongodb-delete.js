cbb5d3bf39d8e09dffabefe0db2b2bcf3838a578
jonathan.cook <nyorksjc@hotmail.com>
2018-03-05 13:28:09 -0500

    Add delete script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -3,8 +3,8 @@
 // Object deconstruction
 const {MongoClient, ObjectID} = require('mongodb');
 
-var obj = new ObjectID();
-console.log(obj);
+// var obj = new ObjectID();
+// console.log(obj);
 
 // var user = {name: 'Jonathan', age: 46};
 //
@@ -16,47 +16,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  // db.collection('Todos').find({
-  //     _id: new ObjectID('5a9b0cc2db59614600ebf821')
-  //   }).toArray().then((docs) => {
-  //   console.log('Todos');
-  //   console.log(JSON.stringify(docs, undefined, 2));
-  // }, (err) => {
-  //   console.log('Unable to fetch todos', err);
-  // });
-
-  // db.collection('Todos').insertOne({
-  //   text: 'Something to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log("Unable to insert todo", err);
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
-
-  // db.collection('Users').insertOne({
-  //   name: 'Jonathan Cook',
-  //   age: 46,
-  //   location: 'Old Saybrook'
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log("Unable to insert user", err);
-  //   }
-  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // delete many
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
   // })
 
-  // db.collection('Todos').find().count().then((count) => {
-  //   console.log(`Todos count : ${count}`);
-  // }, (err) => {
-  //   console.log('Unable to fetch todos', err);
-  // });
+  // db.collection('Users').deleteMany({name: 'Jonathan Cook'}).then((result) => {
+  //   console.log(result);
+  // })
 
-  db.collection('Users').find({name: 'Jonathan Cook'}).toArray().then((docs) => {
-    console.log(JSON.stringify(docs, undefined, 2));
-  }, (err) => {
-    console.log('Unable to fetch todos', err);
-  });
+  // delete one
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // })
+
+  db.collection('Users').findOneAndDelete({_id: new ObjectID("5a9be0eed589191dccba9bf6")}).then((result) => {
+    console.log(result);
+  })
+
+  // find one and delete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // })
 
   //
   // db.close();
