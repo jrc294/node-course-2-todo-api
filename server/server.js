ff08211e04be1e9fd1df7ef6bc7a968dbb270368
Jonathan Cook <jonathan.cook@dealertrack.com>
2018-03-09 16:36:58 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -29,7 +31,21 @@ app.get('/todos', (req, res) => {
   });
 });
 
-
+// GET /todos/1232443
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((doc) => {
+    if (doc) {
+      return res.send({doc});
+    }
+    return res.status(404).send();
+  }).catch((err) => {
+    return res.status(400).send();
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
